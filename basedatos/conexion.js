56db3a272bc5a42931a0ec9f305f78a61d8bc025
pellegrineschi <pellegrineschi86@gmail.com>
2024-01-29 13:49:59 -0300

    conectando a la base de datos
    aplicando arquitectura mvc

--- basedatos/conexion.js
+++ basedatos/conexion.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose"); //importo moongose
 
 const conexion = async() =>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/mi_blog');
+        await mongoose.connect('mongodb://127.0.0.1:27017/mi_blog');// cambie la palabra localhost por 127.0.0.1
 
         console.log('conectado correctamente a la base de datos');
 
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const {conexion} = require('./basedatos/conexion');
+const express = require('express');
+const cors = require('cors');
 
-console.log('app de nose arrancada');
+//inicia la app
+console.log('app de node arrancada');
 
+//conectar a la base de datos
 conexion();
+
+// crear servidor node
+const app = express();
+const puerto = 3900;
+
+//configurar cors
+app.use(cors());
+
+// covertir body a objeto js
+app.use(express.json());
+
+// crear rutas
+
+// crear servidor y escuchar peticiones http
+app.listen(puerto, () =>{
+    console.log('el servidor esta corriendo en el puerto ' + puerto);
+})
