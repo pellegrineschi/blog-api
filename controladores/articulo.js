5f86d22dab66d32babf0d6052850229ccc514e72
pellegrineschi <pellegrineschi86@gmail.com>
2024-02-01 20:41:51 -0300

    crear articulo
    validaciones

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -1,21 +1,37 @@
-const crear = (req, res) =>{
-    let paramametro = req.body;
+const validator = require("validator");
 
-    return res.status(200).json({
-        mensaje: 'metodo para crear',
-        paramametro
-
+const crear = (req, res) => {
+  //recoger parametros por post para guardar
+  let paramametro = req.body;
+  //validar
+  try {
+    let validarTitulo =
+      !validator.isEmpty(paramametro.titulo) &&
+      validator.isLength(paramametro.titulo, { min: 5, max: 15 });
+    let validarContenido = !validator.isEmpty(paramametro.contenido);
+        if(!validarTitulo || !validarContenido){
+            throw new Error('no se a validado la informacion');
+        }
+  } catch (error) {
+    return res.status(400).json({
+      status: error,
+      mensaje: "faltan datos",
     });
-}
+  }
 
+  return res.status(200).json({
+    mensaje: "metodo para crear",
+    paramametro,
+  });
+};
 
-const prueba = (req, res) =>{
-    return res.status(200).json({
-        mensaje: 'soy una accion de prueba'
-    });
-}
+const prueba = (req, res) => {
+  return res.status(200).json({
+    mensaje: "soy una accion de prueba",
+  });
+};
 
 module.exports = {
-    prueba,
-    crear
-}
+  prueba,
+  crear,
+};
