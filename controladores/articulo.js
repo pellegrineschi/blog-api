c56f59c4020adacd78f7cd358c18017b4e40cfd3
pellegrineschi <pellegrineschi86@gmail.com>
2024-02-02 20:17:45 -0300

    crear
    funciona

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -1,45 +1,49 @@
 const validator = require("validator");
 const Articulo = require("../modelos/Articulo");
 
-const crear = (req, res) => {
+const crear = async(req, res) => {
   //recoger parametros por post para guardar
   let parametro = req.body;
   //validar
   try {
     let validarTitulo =
       !validator.isEmpty(parametro.titulo) &&
-      validator.isLength(parametro.titulo, { min: 5, max: 15 });
+      validator.isLength(parametro.titulo, { min: 5, max: 25 });
     let validarContenido = !validator.isEmpty(parametro.contenido);
-    if (!validarTitulo || !validarContenido) {
-      throw new Error("no se a validado la informacion");
-    }
+        if(!validarTitulo || !validarContenido){
+            throw new Error('no se a validado la informacion');
+        }
   } catch (error) {
     return res.status(400).json({
-      status: "error",
+      status: 'error',
       mensaje: "faltan datos",
     });
   }
 
   // crear el objeto a guardar
   const articulo = new Articulo(parametro);
-
+  
   //guardar el articulo en la db
-  articulo.save((error, articuloGuardado) => {
-    if (error || !articuloGuardado) {
-      return res.status(400).json({
-        status: "error",
-        mensaje: "no se guardo el articulo",
-      });
-    }
-
+  try{
+    const articuloGuardado = await articulo.save();
     return res.status(200).json({
-      status: "success",
+      status: 'success',
       articulo: articuloGuardado,
-      mensaje: "guardado con exito",
+      mensaje: 'articulo guardado'
     });
-  });
+  }catch(error){
+    console.error('error al guardar el articulo', error);
+    return res.status(400).json({
+      status: 'error',
+      mensaje: 'no se pudo guardar el archivo'
+    })
+  }
+
+  
 };
 
+
+
 module.exports = {
-  crear,
+  crear
 };
