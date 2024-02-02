207b4b2b00767123be06f7a757dcdf6bc95f1d8b
pellegrineschi <pellegrineschi86@gmail.com>
2024-02-02 19:50:09 -0300

    crear
    mongoose calback

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -1,6 +1,5 @@
 const validator = require("validator");
-const Articulo = require('../modelos/Articulo')
-
+const Articulo = require("../modelos/Articulo");
 
 const crear = (req, res) => {
   //recoger parametros por post para guardar
@@ -10,13 +9,13 @@ const crear = (req, res) => {
     let validarTitulo =
       !validator.isEmpty(parametro.titulo) &&
       validator.isLength(parametro.titulo, { min: 5, max: 15 });
-    let validarContenido = !validator.isEmpty(paramametro.contenido);
-        if(!validarTitulo || !validarContenido){
-            throw new Error('no se a validado la informacion');
-        }
+    let validarContenido = !validator.isEmpty(parametro.contenido);
+    if (!validarTitulo || !validarContenido) {
+      throw new Error("no se a validado la informacion");
+    }
   } catch (error) {
     return res.status(400).json({
-      status: 'error',
+      status: "error",
       mensaje: "faltan datos",
     });
   }
@@ -24,30 +23,23 @@ const crear = (req, res) => {
   // crear el objeto a guardar
   const articulo = new Articulo(parametro);
 
-  
-  
   //guardar el articulo en la db
-  articulo.save((error, articuloGuardado)=>{
-
-    if(error || !articuloGuardado){
+  articulo.save((error, articuloGuardado) => {
+    if (error || !articuloGuardado) {
       return res.status(400).json({
-        status: 'error',
-        mensaje: "no se guardo el articulo"
+        status: "error",
+        mensaje: "no se guardo el articulo",
       });
     }
 
     return res.status(200).json({
-      status: 'success',
+      status: "success",
       articulo: articuloGuardado,
-      mensaje:'guardado con exito'
+      mensaje: "guardado con exito",
     });
-  })
-
-  
+  });
 };
 
-
-
 module.exports = {
-  crear
+  crear,
 };
